0b3028068901daabb68a68ba83f77145feb8004f
Andrii Suliatytskyi <andriythebest134@gmail.com>
2019-10-12 19:52:42 +0200

    Ready to back-end works

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	/*Portfolio*/
    $('.button[filter]').click(function () {
       if($(this).attr('filter') =='all') {
       	if($(this).attr('val') == 'off') {
@@ -19,5 +20,47 @@ $(document).ready(function() {
       		$('.filter > div[filter='+filter+']').show(300);
       	}
    })
+   /*Menu*/
+  $('ul.menu a[href^="#"').click(function () {
+      var target = $(this).attr('href');
+      $('html, body').animate({
+         scrollTop: $(target).offset().top
+      }, 500);
+      $('ul.menu a[href^="#"').css({
+         'color': '#212121'
+      });
+      $(this).css({
+         'color': '#004bee'
+      });
+      return false;
+   });
+
+    $('.menu-icon').click(function () {
+      $('nav').slideToggle(500);
+      $('ul.menu').css({
+         'display': 'flex',
+         'flex-direction': 'column'
+      });
+      if ($('.menu-icon').html() == '<i class="fas fa-bars"></i>') {
+         $(this).html('<i class="fas fa-times"></i>');
+      } else {
+         $(this).html('<i class="fas fa-bars"></i>');
+      }
+   });
+
+
+   /*toTop button*/
+   $(window).scroll(function() {
+   	if ($(this).scrollTop() != 0)
+   		$('#toTop').fadeIn();
+   	else
+   		$('#toTop').fadeOut();
+   });
+   $('#toTop').click(function(){
+   	$('body, html').animate({
+   		scrollTop: 0
+   	}, 800);
+
+	});
 
 });
